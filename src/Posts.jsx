ad228f15aa9fdab6a1ec6c6b51f2a5d77cda855a
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-24 22:58:36 -0500

    made get request with token

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,3 +1,4 @@
+/*
 import { Link } from "react-router-dom";
 
 const Posts = () => {
@@ -11,4 +12,115 @@ const Posts = () => {
   );
 };
 
-export default Posts;
+export default Posts;*/
+
+import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+
+
+function Posts(props) {
+
+  const {
+
+    messages,
+    setMessages,
+
+
+  } = props;
+
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const token = sessionStorage.getItem("token");
+  const tokenOb = JSON.parse(token)
+  const tokenFetch = `Bearer ${tokenOb.token}`
+
+  const fetchInfo = async () => {
+    //setLoading(true)
+
+    try {
+
+     
+
+      const res = await fetch('https://blogapi1200.fly.dev/users/posts', {
+        headers: { Authorization: tokenFetch }
+        
+      })
+
+      //return fetch
+     // const res = await fetch("https://blogapi1200.fly.dev/api/published")
+
+     const messageData = await res.json();
+
+      setMessages(messageData)
+
+    }
+
+    catch (error) {
+      console.error("There has been a problem with your fetch operation:", error);
+      //add error message to dom
+      setError("true")
+
+    }
+    setLoading(false)
+
+  }
+
+
+  useEffect(() => {
+    fetchInfo();
+  }, [])
+
+
+
+  //display error and loading for api call
+
+  if (error) return (
+    <div>
+
+      <p>A network error was encountered</p>
+    </div>
+  )
+
+  if (loading) return <p>Loading...</p>;
+
+
+
+
+  return (
+    <div>
+
+      <div className='postContainer'>
+
+        <div className="postCard">
+
+          {messages.map((index) => {
+            let date = new Date(index.timestamp).toLocaleString()
+
+
+            return (
+
+              <div key={index._id} className="product">
+                <Link to={`post/${index._id}`} state={index._id}>
+                  <div id={index._id} className="card" >
+
+
+                    <h3>{index.title}</h3>
+                    <p>{date}</p>
+                  </div>
+                </Link>
+              </div>
+
+            )
+          })}
+
+        </div>
+      </div>
+    </div>
+  )
+
+
+}
+
+export default Posts
--- src/Router.jsx
+++ src/Router.jsx
@@ -26,6 +26,7 @@ const Router = (props) => {
         element:
           <Posts
           messages={messages}
+          setMessages={setMessages}
           comments={comments}
           setComments={setComments}
           
