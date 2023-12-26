6cd9f2f0d0c49eca147de152f62cd2f26a468eae
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-26 17:28:45 -0500

    added edit

--- src/Edit.jsx
+++ src/Edit.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 
+/*
 const Edit = () => {
   return (
     <div className="error">
@@ -10,5 +10,144 @@ const Edit = () => {
     </div>
   );
 };
+
+export default Edit;*/
+
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useState } from 'react'
+import Header from './Header'
+
+
+
+
+const Edit = (props) => {
+
+  const {
+
+    messages,
+    comments,
+    setComments,
+
+
+  } = props;
+
+  const urlParams = useParams();
+  const postId = urlParams.id
+  const postData = messages.filter((post) => post._id == postId)
+  console.log(postData)
+
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState();
+  const [text, setText] = useState();
+
+  const token = sessionStorage.getItem("token");
+  const tokenOb = JSON.parse(token)
+  const tokenFetch = `Bearer ${tokenOb.token}`
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target).entries());
+    console.log(data.title)
+    console.log(data.text)
+
+
+    //send form data
+    await fetch(`https://blogapi1200.fly.dev/users/edit/${postId}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title: data.title,
+        text: data.text,
+        
+
+      }),
+      headers: {
+        Authorization: tokenFetch,
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+
+      
+      
+      .then((response) => response.json())
+      .then((data) => {
+        navigate('/')
+        //setMessages(data)
+        //maybe set state for a rerender
+      })
+
+
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+
+  }
+
+ /*
+      return (
+        <div className="login-wrapper">
+          <Header/>
+          <h2>Edit Post</h2>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <p>Title</p>
+              <input defaultValue={postData[0].title} type="text" name="title"/>
+            </label>
+            <label>
+              <p>Text</p>
+              <textarea defaultValue={postData[0].text} type="text" name="text"  />
+            </label>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      )
+      */
+  return (
+
+    <div>
+
+      <form id="commentForm" onSubmit={handleSubmit}>
+
+        <label>
+          Title {' '}
+          <input
+            id="title"
+            type="text"
+            name="title"
+            defaultValue={postData[0].title}
+            required
+
+          />
+        </label>
+
+        <label>
+          Text {' '}
+          <textarea
+            id="text"
+            type="text"
+            name="text"
+            defaultValue={postData[0].text}
+            required
+
+          />
+        </label>
+
+        <div className="submitContainer">
+          <input type="submit" value="add a comment" />
+        </div>
+      </form>
+    </div>
+  )
+
+}
+
+
+
 
 export default Edit;
