7163f81e2cd25ff0b21b667cb84c0ac9dec95d68
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-12-25 21:59:53 -0500

    added new post functionality

--- src/NewPost.jsx
+++ src/NewPost.jsx
@@ -1,14 +1,71 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react'
 
-const NewPost = () => {
+
+
+
+
+const NewPost = (props) => {
+  
+  const [title, setTitle] = useState();
+  const [text, setText] = useState();
+
+  const token = sessionStorage.getItem("token");
+  const tokenOb = JSON.parse(token)
+  const tokenFetch = `Bearer ${tokenOb.token}`
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target).entries());
+    console.log(data)
+  
+    await fetch("https://blogapi1200.fly.dev/users/posts/", {
+        method: 'Post',
+        body: JSON.stringify({
+          title: data.title,
+          text: data.text,
+         
+        }),
+        headers: {
+          Authorization: tokenFetch,
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+  
+          //setMessages(data)
+          //redirect to home
+          //maybe set state for a rerender
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+  
+  
+  }
+
   return (
-    <div className="error">
-      <h1>New Post</h1>
-      <Link to="/">
-        You can go back to the home page by clicking here, though!
-      </Link>
+    <div className="login-wrapper">
+      <h2>New Post</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Title</p>
+          <input type="text" name="title" onChange={e => setTitle(e.target.value)} />
+        </label>
+        <label>
+          <p>Text</p>
+          <textarea type="text" name="text" onChange={e => setText(e.target.value)} />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
-  );
-};
+  )
+}
+
+
+
 
 export default NewPost;
